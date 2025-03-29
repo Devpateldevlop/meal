70a141b2610848ef3161d9b5d4a0305ff36a9d5e
Devpateldevlop <devpatel190703@gmail.com>
2025-03-29 16:22:49 +0530

    Initial commit

--- model/meal.js
+++ model/meal.js
@@ -5,16 +5,18 @@ mongoose.connect("mongodb+srv://pdev5771:rxHFzG2xPEkkocvM@cluster0.bso1d.mongodb
 .catch((err) => console.log('MongoDB connection error: ' + err));
 
 const TiffinSchema = new mongoose.Schema({
-    Date: { type: String, required: true },
-    NumberofTiffin: { type: Number, required: true },
-    Axay: { type: String, default: "" },
-    Kaushik_Bhargav: { type: String},
-    Theme: { type: String},
-    Sound: { type: Boolean, default: false },
-    Vibrate: { type: Boolean, default: false },
-    AxayMealPrice: { type: Number, default: 0 },
-    Kaushik_Bhargav_meal_Price  : { type: Number, default: 0 },
-  });
+  Date: { type: Date, required: true },
+  NumberofTiffinAxay: { type: Number, required: true },
+  NumberofTiffin_Kaushik_Bhargav: { type: Number, required: true },
+  Theme: { type: String, default: "sap_horizon" },
+  forLunch: { type: Boolean, default: false },
+  forDinner: { type: Boolean, default: false },
+  AxayMealPrice: { type: Number, default: 0 },
+  Kaushik_Bhargav_meal_Price: { type: Number, default: 0 },
+  Sound: { type: Boolean, default: false },
+  Vibrate: { type: Boolean, default: false }
+});
+ 
 
 const meal = mongoose.model('meal', TiffinSchema,'meal');
 module.exports = meal;
